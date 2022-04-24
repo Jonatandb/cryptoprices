6f199ca144dba38daaa2901f1b6cb32ce3795236
Jonatandb <jonatandb@gmail.com>
2022-04-24 00:20:36 -0300

    Styling main page

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import './App.css'
 
 function App() {
   return (
-    <div className='container-fluid'>
-      <h1 className='text-center'>Cryptocurrency Prices</h1>
+    <div className='container-fluid text-center mt-4'>
+      <h1 className='mb-4'>Cryptocurrency Prices</h1>
     </div>
   )
 }
