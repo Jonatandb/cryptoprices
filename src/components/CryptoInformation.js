f70cca27aa6fcf195712e55e7d71c431f638a870
Jonatandb <jonatandb@gmail.com>
2022-04-24 00:37:58 -0300

    Added functionality to get Crytpo information from API

--- src/components/CryptoInformation.js
+++ src/components/CryptoInformation.js
@@ -2,13 +2,42 @@ import { useEffect, useRef, useState } from 'react'
 
 const CryptoInformation = () => {
   const inputRef = useRef(null)
+  const componentMounted = useRef(false)
   const [search, setSearch] = useState('')
+  const [cryptoData, setCryptoData] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
+  const [error, setError] = useState(null)
 
   useEffect(() => {
+    componentMounted.current = true
+
     if (inputRef.current) {
       inputRef.current.focus()
     }
+
+    getCrytpoData()
+
+    return () => {
+      componentMounted.current = false
+    }
   }, [])
+
+  const getCrytpoData = async () => {
+    // Get the crypto data from CoinGecko API:
+    const url = `https://api.coingecko.com/api/v3/coins/`
+    try {
+      setIsLoading(true)
+      const response = await fetch(url)
+      const data = await response.json()
+      if (componentMounted.current) {
+        setCryptoData(data)
+      }
+    } catch (error) {
+      setError(error)
+    } finally {
+      setIsLoading(false)
+    }
+  }
 
   const onSearchChanged = event => {
     setSearch(event.target.value)
@@ -24,17 +53,27 @@ const CryptoInformation = () => {
         ref={inputRef}
         className='form-control'
       />
-      <table className='table table-dark table-hover mt-3'>
-        <thead>
-          <tr>
-            <th>Ranking</th>
-            <th>Name</th>
-            <th>Symbol</th>
-            <th>Price</th>
-            <th>Price 24h</th>
-          </tr>
-        </thead>
-      </table>
+      {isLoading ? (
+        <div className='mt-5'>
+          <span>Loading data...</span>
+        </div>
+      ) : error ? (
+        <div className='mt-5'>
+          <span>{error.message}</span>
+        </div>
+      ) : (
+        <table className='table table-dark table-hover mt-3'>
+          <thead>
+            <tr>
+              <th>Ranking</th>
+              <th>Name</th>
+              <th>Symbol</th>
+              <th>Price</th>
+              <th>Price 24h</th>
+            </tr>
+          </thead>
+        </table>
+      )}
     </>
   )
 }
